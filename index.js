4dee1784c088f0ecdb6fdee1cef09dd781e8ffe0
Shaiju Edakulangara <eshaiju@gmail.com>
2019-12-24 13:33:46 +0300

    User signup and login

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const login = require('./routes/login');
+const app = express();
+
+mongoose.connect('mongodb://localhost/spms')
+    .then(() => console.log('connected to MongoDB.....'))
+    .catch(err => console.error(err.message))
+
+app.use(express.json());
+app.use('/api/users', users);
+app.use('/api/login', login);
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ models/user.js
@@ -0,0 +1,57 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    emp_id: {
+        type: String,
+        minlength: 3,
+        maxlength: 10,
+        unique: true
+    },
+    designation: {
+        type: String
+    }
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+    return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required(),
+        emp_id: Joi.string().min(3).max(10).required(),
+        designation: Joi.string()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
